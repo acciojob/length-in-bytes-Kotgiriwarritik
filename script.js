a5d264ccf39c1aa52654539fd9993cd0d42d513a
Ritik kotgiriwar <87945415+Kotgiriwarritik@users.noreply.github.com>
2023-12-18 14:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const byteSize = (str) => {
-  // write your code here
+  // write your code here	
+	 const blob = new Blob([str]);
+
+  // Get the size property of the Blob object, which gives the size in bytes
+  return blob.size;
 };
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
